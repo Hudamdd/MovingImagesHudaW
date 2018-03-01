5879c16568a43c28207d3c452a6c24651293fbb6
Hudamdd <36892645+Hudamdd@users.noreply.github.com>
2018-03-01 10:46:10 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,10 @@
+---Title: MovingImages
+-- Name: Huda Woyessa
+-- Course: ICS2O/3C
+-- This program displays a beetleship moving 
+--across the screen as well as another objecat moving in a dirrent direction
+
+print("***Testing beetleship")
+--hide the status bad 
+dispay.setStatusBar(display.HiddenStatusBar)
+
